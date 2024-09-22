bd95d8c8530f83783f5667bbe512c9e562df33b7
Kulisara Thongngamkhum <64160233@go.buu.ac.th>
2024-09-22 22:42:02 +0700

    entity order & swagger

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,1 +1,16 @@
-export class Order {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  productId: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  status: string;
+}
